8a7e3c09b1c64499026b3c3ad9874ef323d967da
esthervega <esther.vega@gmail.com>
2015-04-12 21:16:20 +0100

    First Action and config

--- my-first-app/app/controllers/Application.scala
+++ my-first-app/app/controllers/Application.scala
@@ -8,5 +8,9 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
+  
+  def nameWelcome(name: String) = Action {
+    Ok("Hello and welcome! " + name)
+  }
 
 }
